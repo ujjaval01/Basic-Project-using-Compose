5748ad879383c0a948ca56e6fc0b5f303751a329
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-30 22:57:51 +0530

    Implement `LazyColumn` and `verticalScroll` examples for category lists in `LazyColumnPractice.kt`.

--- app/src/main/java/com/uv/practiceproject/practice/LazyColumnPractice.kt
+++ app/src/main/java/com/uv/practiceproject/practice/LazyColumnPractice.kt
@@ -6,12 +6,18 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -26,6 +32,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uv.practiceproject.R
 
+
+
+
 @Composable
 fun BlogCategory(img: Int, title: String, subtitle: String, salary: String, joining: String, modifier: Modifier = Modifier) {
     Card(
@@ -33,7 +42,8 @@ fun BlogCategory(img: Int, title: String, subtitle: String, salary: String, join
         elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
         border = BorderStroke(0.5.dp, colorResource(R.color.teal_700))
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically){
+        Row(verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.padding(start = 10.dp)){
             Image(
                 painter = painterResource(id = img),
                 contentDescription = null,
@@ -65,6 +75,7 @@ private fun RowScope.Extracted(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+
                 .padding(5.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
@@ -95,19 +106,49 @@ private fun RowScope.Extracted(
 
     }
 }
-
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun BlogCategoryPreview() {
-    Column {
-        categoryList().forEach { item ->
-            BlogCategory(
-                img = item.img,
-                title = item.title,
-                subtitle = item.subtitle,
-                salary = item.salary,
-                joining = item.joining
-            )
+fun CategoryListScreenUsingLazyLayout() {
+    Scaffold { innerPadding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(categoryList()) { item ->
+                BlogCategory(
+                    img = item.img,
+                    title = item.title,
+                    subtitle = item.subtitle,
+                    salary = item.salary,
+                    joining = item.joining
+                )
+            }
+        }
+    }
+}
+
+
+//@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun CategoryListScreenUsingVerticalScroll(modifier: Modifier = Modifier) {
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+        )
+        {
+            categoryList().map { item ->
+                BlogCategory(
+                    img = item.img,
+                    title = item.title,
+                    subtitle = item.subtitle,
+                    salary = item.salary,
+                    joining = item.joining
+                )
+            }
         }
     }
 }
