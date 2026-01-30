1b879a798ea29f7827afa2d1b73961e909cafbab
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-30 22:59:33 +0530

    Update `MainActivity` to include `CategoryListScreenUsingLazyLayout` and refactor layout imports.

--- app/src/main/java/com/uv/practiceproject/MainActivity.kt
+++ app/src/main/java/com/uv/practiceproject/MainActivity.kt
@@ -6,25 +6,35 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.uv.practiceproject.apiCallingPager.ProductListScreen
 import com.uv.practiceproject.apiCallingPager.ProductViewModel
-import com.uv.practiceproject.practice.CountScreen
+import com.uv.practiceproject.practice.BlogCategory
+import com.uv.practiceproject.practice.Category
 import com.uv.practiceproject.practice.CustomListViewPractice
-import com.uv.practiceproject.practice.CustomListViewPracticePreview
-import com.uv.practiceproject.practice.TextCompo
+import com.uv.practiceproject.practice.categoryList
 import com.uv.practiceproject.ui.theme.PracticeProjectTheme
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+
+import androidx.compose.foundation.lazy.items
+import com.uv.practiceproject.practice.CategoryListScreenUsingLazyLayout
+
 
 class MainActivity : ComponentActivity() {
 
@@ -36,8 +46,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticeProjectTheme {
 
+                CategoryListScreenUsingLazyLayout()
+//                CategoryListScreenUsingVerticalScroll()
 //               TextCompo("Ujjaval")
-                CustomListViewPracticePreview()
+//                CustomListViewPracticePreview()
+
+
 
                 @Composable
                 fun CustomListViewPracticePreview(modifier: Modifier = Modifier) {
@@ -68,7 +82,6 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
 @Composable
 fun ToastDemo(modifier: Modifier = Modifier) {
@@ -84,4 +97,8 @@ fun ToastDemo(modifier: Modifier = Modifier) {
     }) {
         Text(text = "Toast")
     }
+}
+
+
+
 }
