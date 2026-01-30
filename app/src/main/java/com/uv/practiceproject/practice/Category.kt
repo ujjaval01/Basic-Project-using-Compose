594935689e692490d448c1a234eba021ce052204
Ujjaval Saini <sainiujvl@gmail.com>
2026-01-30 22:57:17 +0530

    Format `Category` data class for better readability.

--- app/src/main/java/com/uv/practiceproject/practice/Category.kt
+++ app/src/main/java/com/uv/practiceproject/practice/Category.kt
@@ -1,3 +1,9 @@
 package com.uv.practiceproject.practice
 
-data class Category(val img: Int, val title: String, val subtitle: String, val salary: String, val joining: String)
+data class Category(
+    val img: Int,
+    val title: String,
+    val subtitle: String,
+    val salary: String,
+    val joining: String
+)
